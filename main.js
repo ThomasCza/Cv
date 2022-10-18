51b76a04c4e7ce775b8ce07cbdf163408fc18505
Thomas Czajkowski <thomascza33@gmail.com>
2022-10-18 16:46:51 +0200

    Mise au point

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $('.header-right a').click(function (){
+      $('.hamburger-menu').show();
+    });
+  
+    $('.hamburger-menu a').click(function (){
+      $('.hamburger-menu').hide();
+    });
+  
+  });
